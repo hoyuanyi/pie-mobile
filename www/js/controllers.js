37f95bf8ef2bb61a8e8dfef6e3d3d3d45d5f5f51
hoyuanyi <yuanyi1122@hotmail.com>
2015-09-28 12:17:13 +0800

    Add register.html

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -31,7 +31,33 @@ angular.module('pie')
 
 })
 
-.controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService) {
+.controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService, $ionicModal) {
+  
+  $ionicModal.fromTemplateUrl('templates/register.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openRegisterModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeRegisterModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
   $scope.data = {};
  
   $scope.login = function(data) {
